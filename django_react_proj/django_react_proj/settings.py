d9abb31c52d15f6ae47aea5f3ced870d68f5daa2
Kenny Lepping <kennylepping@gmail.com>
2021-02-28 03:00:46 -0500

    Changed static file settings

--- django_react_proj/django_react_proj/settings.py
+++ django_react_proj/django_react_proj/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+PROJECT_DIR = Path(__file__).resolve().parent
+BASE_DIR = PROJECT_DIR.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -122,6 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATICFILES_DIRS = [PROJECT_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
 
 # CORS
